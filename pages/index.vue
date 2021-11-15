d99aae1ca4e735c4d6f27ad291d2682b738fd3f3
ybb322 <masalevich98@gmail.com>
2021-11-15 15:46:45 +0300

    Revert "Auxiliary commit to revert individual files from 89643e9b1c7ee069d91a8516bb915bb383ee24b8"
    This reverts commit eb936f01f54cbc0f5a4d1780c752ee225ee30696.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="root">
-    <h2>hi</h2>
-  </div>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <h1>index page</h1>
+    </v-col>
+  </v-row>
 </template>
