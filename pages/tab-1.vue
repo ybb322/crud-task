8f4b37193c917f355cf63069fdd5ed8fc8c10a97
ybb322 <masalevich98@gmail.com>
2021-11-17 15:19:03 +0300

    code style edited with eslint

--- pages/tab-1.vue
+++ pages/tab-1.vue
@@ -1,173 +1,275 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="12" md="12">
-        <h1>tab-1</h1>
+  <v-container class="wrapper">
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="12" md="12">
+        <h1 class="mb-5">{{ usersTabTitle }}</h1>
+        <v-row justify="start" no-gutters>
+          <v-col cols="4" sm="2" md="1">
+            <v-btn block class="blue-grey darken-2 mb-5" @click="createItem"
+              >New item</v-btn
+            >
+          </v-col>
+        </v-row>
         <v-data-table
-        :headers="headers"
-        :items="apiData"
-        :items-per-page="10"
-        class="elevation-1"
-    >
-      <template v-slot:[`item.actions`]="{item}">
-        <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
-    </template></v-data-table>
-    </v-col>
-  <v-dialog v-model="editDialog" width="500" class="edit-dialog">
-    <v-card>
-      <v-card-title>Edit item</v-card-title>
-      <v-text-field label="ID" v-model="editedItem.id"></v-text-field>
-      <v-text-field label="Name" v-model="editedItem.name"></v-text-field>
-      <v-text-field label="Username" v-model="editedItem.username"></v-text-field>
-      <v-text-field label="Email" v-model="editedItem.email"></v-text-field>
-      <v-text-field label="City" v-model="editedItem.address.city"></v-text-field>
-      <v-text-field label="Street" v-model="editedItem.address.street"></v-text-field>
-      <v-text-field label="Suite" v-model="editedItem.address.suite"></v-text-field>
-    </v-card>
-    <v-btn class="primary" @click="saveChanges">Save</v-btn>
-  </v-dialog>
-  </v-row>
+          :headers="headers"
+          :items="apiData"
+          :items-per-page="-1"
+          class="elevation-1"
+        >
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small class="mr-2" @click="editItem(item)">
+              mdi-pencil
+            </v-icon>
+            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+          </template></v-data-table
+        >
+      </v-col>
+      <v-dialog v-model="editDialog" width="400" class="edit-dialog">
+        <v-card max-width="400" class="edit-dialog-card dialog-card">
+          <v-row justify="center">
+            <v-card-title class="mt-3">Edit item</v-card-title>
+            <v-col cols="10">
+              <v-text-field
+                class="shrink"
+                label="ID"
+                v-model="editedItem.id"
+              ></v-text-field>
+              <v-text-field
+                label="Name"
+                v-model="editedItem.name"
+              ></v-text-field>
+              <v-text-field
+                label="Username"
+                v-model="editedItem.username"
+              ></v-text-field>
+              <v-text-field
+                label="Email"
+                v-model="editedItem.email"
+              ></v-text-field>
+              <v-text-field
+                label="City"
+                v-model="editedItem.address.city"
+              ></v-text-field>
+              <v-text-field
+                label="Street"
+                v-model="editedItem.address.street"
+              ></v-text-field>
+              <v-text-field
+                label="Suite"
+                v-model="editedItem.address.suite"
+              ></v-text-field>
+              <v-row justify="center" class="mb-3">
+                <v-col cols="4">
+                  <v-btn block color="blue-grey darken-4" @click="saveChanges"
+                    >Save</v-btn
+                  >
+                </v-col></v-row
+              >
+            </v-col></v-row
+          >
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="newItemDialog" width="400">
+        <v-card max-width="400" class="dialog-card">
+          <v-row justify="center">
+            <v-card-title class="mt-3">New item</v-card-title>
+            <v-col cols="10">
+              <v-text-field label="ID" v-model="newItem.id"></v-text-field>
+              <v-text-field label="Name" v-model="newItem.name"></v-text-field>
+              <v-text-field
+                label="Username"
+                v-model="newItem.username"
+              ></v-text-field>
+              <v-text-field
+                label="Email"
+                v-model="newItem.email"
+              ></v-text-field>
+              <v-text-field
+                label="City"
+                v-model="newItem.address.city"
+              ></v-text-field>
+              <v-text-field
+                label="Street"
+                v-model="newItem.address.street"
+              ></v-text-field>
+              <v-text-field
+                label="Suite"
+                v-model="newItem.address.suite"
+              ></v-text-field>
+              <v-row justify="center" class="mb-3">
+                <v-col cols="4">
+                  <v-btn block color="blue-grey darken-4" @click="saveNewItem"
+                    >Save</v-btn
+                  >
+                </v-col></v-row
+              >
+            </v-col></v-row
+          >
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-    data () {
-        return {
-        headers: [
-                {
-                text: 'ID',
-                value:'id'
-                },
-                {
-                text: 'Name',
-                value:'name'
-                },
-                {
-                text: 'Username',
-                value:'username'
-                },
-                {
-                text: 'Email',
-                value:'email'
-                },
-                {
-                text: 'City',
-                value:'address.city'
-                },
-                {
-                text: 'Street',
-                value:'address.street'
-                },
-                {
-                text: 'Suite',
-                value:'address.suite'
-                },
-                {
-
-                text: 'Phone',
-                value:'phone'
-                },
-                {
-                text:'Actions',
-                value:'actions'
-                }
-        ],
-        apiData: [],
-        editDialog: false,
-        editedItem: {
-          id: '',
-          name: '',
-          username:'',
-          email:'',
-          address: {
-            city: '',
-            street:'',
-            suite:'',
-          },
+  data() {
+    return {
+      headers: [
+        {
+          text: "ID",
+          value: "id",
         },
-        }
-    },
-    mounted() {
-
+        {
+          text: "Name",
+          value: "name",
+        },
+        {
+          text: "Username",
+          value: "username",
+        },
+        {
+          text: "Email",
+          value: "email",
+        },
+        {
+          text: "City",
+          value: "address.city",
+        },
+        {
+          text: "Street",
+          value: "address.street",
+        },
+        {
+          text: "Suite",
+          value: "address.suite",
+        },
+        {
+          text: "Actions",
+          value: "actions",
+        },
+      ],
+      usersTabTitle: "Users data",
+      apiData: [],
+      newItemDialog: false,
+      editDialog: false,
+      editedItem: {
+        id: "",
+        name: "",
+        username: "",
+        email: "",
+        address: {
+          city: "",
+          street: "",
+          suite: "",
+        },
+      },
+      newItem: {
+        id: "",
+        name: "",
+        username: "",
+        email: "",
+        address: {
+          city: "",
+          street: "",
+          suite: "",
+        },
+      },
+      defaultItem: {
+        id: "",
+        name: "",
+        username: "",
+        email: "",
+        address: {
+          city: "",
+          street: "",
+          suite: "",
+        },
+      },
+    };
+  },
+  mounted() {
     // Getting data from remote server
     const vm = this;
     axios
-    .get('https://jsonplaceholder.typicode.com/users')
-    .then(response => (this.apiData = response.data))
-    .then (() => {
-    })
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => (this.apiData = response.data))
+      .then(() => {});
   },
   methods: {
-    deleteItem (item) {
+    deleteItem(item) {
       // Deleting data on server
       axios
-      .delete(`https://jsonplaceholder.typicode.com/users/${item.id}`)
-      .then(response => {
-        console.log(response)
-        if (response.status == 200) {
-      // If successful => deleting data locally
-          this.apiData.splice(this.apiData.indexOf(item),1)
-          console.log (`Item ${item.id} was deleted from the remote storage!`)
-        }
-      })
+        .delete(`https://jsonplaceholder.typicode.com/users/${item.id}`)
+        .then((response) => {
+          console.log(response);
+          if (response.status == 200) {
+            // If successful => deleting data locally
+            this.apiData.splice(this.apiData.indexOf(item), 1);
+            console.log(`Item ${item.id} was deleted from the remote storage!`);
+          }
+        });
     },
-    editItem (item) {
+    editItem(item) {
       // Editing data locally
-      this.editedIndex = this.apiData.indexOf(item)
-      this.editedItem.id = item.id
+      this.editedIndex = this.apiData.indexOf(item);
+      this.editedItem.id = item.id;
       this.editedItem.name = item.name;
       this.editedItem.username = item.username;
       this.editedItem.email = item.email;
-      this.editedItem.address.city = item.address.city
-      this.editedItem.address.street = item.address.street
-      this.editedItem.address.suite = item.address.suite
-      this.editDialog = true
+      this.editedItem.address.city = item.address.city;
+      this.editedItem.address.street = item.address.street;
+      this.editedItem.address.suite = item.address.suite;
+      this.editDialog = true;
     },
-    saveChanges () {
+    saveChanges() {
       // Saving edited data locally
-      this.apiData[this.editedIndex].id = this.editedItem.id
-      this.apiData[this.editedIndex].name = this.editedItem.name
-      this.apiData[this.editedIndex].username = this.editedItem.username
-      this.apiData[this.editedIndex].email = this.editedItem.email
-      this.apiData[this.editedIndex].address.city = this.editedItem.address.city
-      this.apiData[this.editedIndex].address.street = this.editedItem.address.street
-      this.apiData[this.editedIndex].address.suite = this.editedItem.address.suite
+      this.apiData[this.editedIndex].id = this.editedItem.id;
+      this.apiData[this.editedIndex].name = this.editedItem.name;
+      this.apiData[this.editedIndex].username = this.editedItem.username;
+      this.apiData[this.editedIndex].email = this.editedItem.email;
+      this.apiData[this.editedIndex].address.city =
+        this.editedItem.address.city;
+      this.apiData[this.editedIndex].address.street =
+        this.editedItem.address.street;
+      this.apiData[this.editedIndex].address.suite =
+        this.editedItem.address.suite;
       // Updating saved data on server
       axios
-      .patch(`https://jsonplaceholder.typicode.com/users/${this.apiData.indexOf(this.apiData[this.editedIndex + 1])}`, {
-        body: {
-          id: `${this.editedItem.id}`,
-          name: `${this.editedItem.name}`,
-          username: `${this.editedItem.username}`,
-          email: `${this.editedItem.id}`,
-          address: {
-            city: `${this.editedItem.address.city}`,
-            street: `${this.editedItem.address.street}`,
-            suite: `${this.editedItem.address.suite}`,
+        .patch(
+          `https://jsonplaceholder.typicode.com/users/${this.apiData.indexOf(
+            this.apiData[this.editedIndex + 1]
+          )}`,
+          {
+            body: {
+              id: `${this.editedItem.id}`,
+              name: `${this.editedItem.name}`,
+              username: `${this.editedItem.username}`,
+              email: `${this.editedItem.id}`,
+              address: {
+                city: `${this.editedItem.address.city}`,
+                street: `${this.editedItem.address.street}`,
+                suite: `${this.editedItem.address.suite}`,
+              },
+            },
           }
-        }
-      })
-      .then(response => console.log (response))
+        )
+        .then((response) => console.log(response));
       this.editDialog = false;
-    }
+    },
+    createItem() {
+      this.newItemDialog = true;
+    },
+    saveNewItem() {
+      this.apiData.push(this.newItem);
+      axios
+        .post("https://jsonplaceholder.typicode.com/users", this.newItem)
+        .then((response) => console.log(response));
+      this.newItemDialog = false;
+      this.newItem = Object.assign({}, this.defaultItem);
+    },
   },
-}
+};
 </script>
-
-
-<style>
-
-</style>
